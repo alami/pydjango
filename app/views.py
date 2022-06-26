058a64172bb26cf37269b44c9b2ffdbb26285af6
alami <alami@km.ru>
2022-06-26 15:11:20 +0500

    python .\manage.py startapp app

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+from datetime import datetime
+
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse
+
+from django.conf import settings
+
+def test_view(request):
+    return HttpResponse(reverse('test'))
+
+def home_view(request):
+    return HttpResponse('Home from Django!  ')
+
+
+def contact_view(request):
+    msg = f'Contact email: {settings.CONTACT_EMAIL}'
+    return HttpResponse(msg)
+
+def hello_view(request):
+    name = request.GET.get('name', 'Unknown')
+    msg = f'Hi {name} from Django!  '
+    return HttpResponse(msg)
+
+# 127.0.0.1:8000/since/2020-01-01/
+def since_view(request,date):
+    try:
+        dt=datetime.strptime(date,'%Y-%m-%d')
+    except:
+        raise Http404()
+    daysi = (datetime.now() - dt).days
+    msg = f'Contact date: {daysi}'
+    return HttpResponse(msg)
+
+
+
+
--- pydjango/urls.py
+++ pydjango/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse
+
+from app.views import hello_view, test_view, home_view, contact_view, since_view
+
+class DataConverter:
+    redux = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_view, name='hello'), ##http://127.0.0.1:8000/hello?name=Alisher
+    path('test33/', test_view, name='test'),  ##http://127.0.0.1:8000/test33/
+    path('', home_view, name='home'),
+    path('contact/', contact_view, name='contact'),
+    path('since/<date>/', since_view, name='since'),
 ]
+
+print(reverse('hello'))
