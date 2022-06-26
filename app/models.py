f30c01ecb81a8e5f5745a3c5dcd75fb676575282
alami <alami@km.ru>
2022-06-26 19:33:09 +0500

    migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import TextChoices
+
+
+class Country(TextChoices):
+    ENGLAND = "ENG", "Англия"
+    RUSSIA = "RU", "Россия"
+    ESP = "ESP", "Испания"
+
+
+class Team(models.Model):
+    name = models.TextField()
+    country = models.TextField()
+    city = models.TextField()
